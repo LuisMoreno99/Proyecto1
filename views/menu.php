5368b94e9b5d688aaf5849986b48fe5a3c197c67
MrLuis <lmoreno.personal769@gmail.com>
2019-12-09 13:22:46 -0500

    Agregue un menu con estilos y otras carpetas

--- views/menu.php
+++ views/menu.php
@@ -1,4 +1,24 @@
-<nav>
+<?php
+    include("heads.php");
+?>
+<nav class="menuCSS3">
+    <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Empleados</a>
+        <ul>
+          <li><a href="#">Juan</a></li>
+          <li><a href="#">Paco</a></li>
+          <li><a href="#">Ramón</a></li>
+          <li><a href="#">María</a></li>
+        </ul>
+      </li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Contacto</a>
+        <ul>
+          <li><a href="#">Email</a></li>
+          <li><a href="#">Mapa</a></li>
+        </ul>
 
-
-</nav>
+      </li>
+    </ul>
+  </nav>
